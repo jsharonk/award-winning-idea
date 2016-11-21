24cf3646de89cd423e49dd4469eee7b10a28d0b5
Julie Busch <heyjuliebusch@gmail.com>
2016-11-21 15:42:52 -0500

    more external server

--- external-server/index.js
+++ external-server/index.js
@@ -1,3 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
 
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.listen('7599', function() {
+  console.log('server listening on port 7599');
+});
+
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
