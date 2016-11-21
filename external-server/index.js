9387d964f94906c641a92df6e256c3f2da8ebc90
Julie Busch <heyjuliebusch@gmail.com>
2016-11-21 17:47:14 -0500

    finished server

--- external-server/index.js
+++ external-server/index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var player = require('play-sound')(opts = {});
+var inquirer = require('inquirer');
 
 app.engine('html', nunjucks.render);
 app.set('view engine', 'html');
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var message = {text: 'Your stuff is undisturbed!'};
 
 app.listen('7599', function() {
-  console.log('server listening on port 7599');
+  console.log('theft alert system listening on port 7599!');
 });
 
 app.get('/', function(req, res, next) {
@@ -22,23 +23,34 @@ app.get('/', function(req, res, next) {
   res.render('index', {message: message});
 });
 
-app.post('/', function(req, res, next) {
-  res.send(req.body.response);
-});
+// app.post('/', function(req, res, next) {
+//   res.send(req.body.response);
+// });
 
 // app.get('/ohno', function(req, res, next) {
 //   res.render('index', {message: message});
 // });
 
 app.post('/snatched', function(req, res, next) {
-  console.log('Someone took your food!!');
+  console.log('Someone took your stuff!!');
   var audio = player.play('Alarm.mp3', function(err) {
-    if (err) console.log('Sound alert error!');
+    if (err && !err.killed) {
+      console.log('Sound alert error!');
+      audio.kill();
+    }
   });
   setTimeout(function() {
     audio.kill();
+    inquirer.prompt([{
+    type: 'input', name: 'prompt', message: 'Tell that thief to step off!!'}])
+    .then(function(answer) {
+      console.log('answer sent!');
+      res.send(answer.prompt);
+    });
   }, 5000);
+
 
   //message = {text: 'Someone is messing with your stuff!!!'}
   //res.redirect('/ohno');
 });
+
